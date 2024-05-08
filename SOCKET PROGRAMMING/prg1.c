22357682f00162dd061f729414529ae9c8f686ed
Dcoding15 <debrishibiswas2003@gmail.com>
2024-05-08 13:26:25 +0530

    Update 08-05-2024

---
+++ SOCKET PROGRAMMING/prg1.c
@@ -0,0 +1,15 @@
+#include<sys/socket.h>  //header file for socket programming
+int main() {
+
+/*
+    Socket Creation             : socket(int domain, int type, int protocol);
+    Socket Binding              : bind(int sockfd, struct sockaddr * myaddr, scklen_t addrlen);
+    Socket Listening            : listen(int sockfd, int backlog);
+    Socket Acccepting           : accept(int sockfd, struct sockaddr * addr, socklen_t * addrlen);
+    Socket Connection           : connect(int sockfd struct sockaddr * serv_addr, socklen_t addrlne);
+*/
+
+	socket()
+
+    return 0;
+}
